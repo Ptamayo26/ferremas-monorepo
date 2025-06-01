299bebc79390463cdfaebcdd350ba6e4b8fd75b7
Paloma Tamayo <p.tamayo.segura@gmail.com>
2025-06-01 19:13:00 -0400

    inicializado proyecto frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      minHeight: '100vh',
+      fontSize: '3rem',
+      fontWeight: 'bold'
+    }}>
+      Ferremas
+    </div>
+  )
+}
+
+export default App
